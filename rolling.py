43957cfb8a187fa1b78fcd44030ad025a9e0a2e4
SamuelDiai <diai.samuel@gmail.com>
2021-01-18 23:51:59 +0100

    .

--- rolling.py
+++ rolling.py
@@ -2,6 +2,8 @@ from trainer import Trainer
 from dataloader import Dataset
 import torch
 import numpy as np
+import pandas as pd
+
 class Rolling:
     def __init__(self, rolling_dict, params_dataset, params_data_generation, params_model):
             self.__dict__.update((k, v) for k, v in rolling_dict.items())
