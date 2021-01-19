d4e09059a7e9a07f3f701d3fa7cb43f25a3f82df
SamuelDiai <diai.samuel@gmail.com>
2021-01-19 01:29:38 +0100

    .

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import os
 def save_or_append(df_output, path):
     if os.path.isfile(path):
         df = pd.read_csv(path)
-        df.append(df_output, ignore_index = True)
+        df = df.append(df_output, ignore_index = True)
     else :
         df = df_output
     df.to_csv(path, index = False)
