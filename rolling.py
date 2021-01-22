43cdd49551fedcf991ca6f57811bd72f9020578b
SamuelDiai <diai.samuel@gmail.com>
2021-01-22 14:15:21 +0100

    .

--- rolling.py
+++ rolling.py
@@ -54,9 +54,9 @@ class Rolling:
 
             # Create model
             mod = Trainer(self.params_model, dict_generators)
-            self.nb_parameters = sum(p.numel() for p in mod.model.parameters())
+            #self.nb_parameters = sum(p.numel() for p in mod.model.parameters())
             # Train model
-            mod.training()
+            #mod.training()
 
             # Do forecast
             mod.complete_forecast()
